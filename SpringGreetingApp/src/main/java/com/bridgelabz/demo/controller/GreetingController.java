4c5ffe7ccba750ec828a16979037b33589d0b010
R-scan1 <deepr7590@gmail.com>
2025-02-22 16:40:40 +0530

    [Raj] add. using service layer to give hello world message

--- SpringGreetingApp/src/main/java/com/bridgelabz/demo/controller/GreetingController.java
+++ SpringGreetingApp/src/main/java/com/bridgelabz/demo/controller/GreetingController.java
@@ -1,30 +1,39 @@
 package com.bridgelabz.demo.controller;
 
+import com.bridgelabz.demo.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greet")
 public class GreetingController {
 
-    private final String message = "Hello from BridgeLabz";
-
+    
+    private final GreetingService greetingService;
+    
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+	
     @GetMapping
     public String getGreeting() {
-        return "{\"GET\": \"" + message + "\"}";
+        return "GET: "+greetingService.getGreetingMessage();
     }
 
     @PostMapping
     public String postGreeting() {
-        return "{\"POST\": \"" + message + "\"}";
+        return "POST: "+greetingService.getGreetingMessage();
     }
 
-    @PutMapping("/{name}")
-    public String putGreeting(@PathVariable String name) {
-        return "{\"PUT\": \"" + message + "\"}";
+    @PutMapping
+    public String putGreeting() {
+        return "PUT: "+greetingService.getGreetingMessage();
     }
 
-    @DeleteMapping("/{name}")
-    public String deleteGreeting(@PathVariable String name) {
-        return "{\"DELETE\": \"" + message + "\"}";
+
+    @DeleteMapping
+    public String deleteGreeting() {
+        return "DELETE: "+greetingService.getGreetingMessage();
     }
 }
---
+++ SpringGreetingApp/src/main/java/com/bridgelabz/demo/service/GreetingService.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    // Method to return a simple greeting message
+    public String getGreetingMessage() {
+        return "Hello World";
+    }
+}
